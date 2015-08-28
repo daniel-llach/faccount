5101a848d88ad42817c4f447108852bdaccd4098
daniel-llach <daniel.llach@gmail.com>
2015-08-28 11:41:23 -0300

    db ip fixed

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ module.exports.url_daccount = 'http://localhost:4000';
 /*
  * CONFIG DATABASE
  */
- module.exports.db_host = '192.168.0.150';
+ module.exports.db_host = '186.67.218.180';
  module.exports.db_user = 'root';
  module.exports.db_password = 'foris123'
  module.exports.db_database = 'foris_account'
